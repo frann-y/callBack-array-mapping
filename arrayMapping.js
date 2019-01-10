93b12b398bd6dd06992b41456a3ed1acd461e9e4
frann-y <34494726+frann-y@users.noreply.github.com>
2019-01-10 17:50:45 -0330

    Add files via upload
    completed

---
+++ arrayMapping.js
@@ -0,0 +1,18 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+
+var result = input.map(function(factors) {
+  var value = Math.sqrt(Math.pow(factors.x, 2) + Math.pow(factors.y, 2));
+  return value;
+});
+
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
+
+
+
